b8dcd7fa04db4bf42c0132ce517f53b6d36ed288
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2024-06-27 10:29:08 +0300

    chore: Update server.js to serve static files from the correct directory

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/DMRS'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/DMRS/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
